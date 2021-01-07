4a75bb93a2d1454f2dfd16903b45a2383f81876c
Karina Blinova <blinova.k@ati.su>
2021-01-07 18:01:21 +0300

    first commit

---
+++ Apple store/Apple store/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple store
+//
+//  Created by Karina Blinova on 07.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
